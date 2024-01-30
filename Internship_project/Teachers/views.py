34020e6292a6fe9b431c4b2666718dacb0db0c13
pshycodr <115141578+pshycodr@users.noreply.github.com>
2024-01-30 09:45:35 +0000

    add pyrebase

--- Internship_project/Teachers/views.py
+++ Internship_project/Teachers/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+import pyrebase
 
 # Create your views here.
+firebaseConfig = {
+  "apiKey": "AIzaSyA8P2BGbRuJz8Nz_goXxGmdutsgmI670PI",
+  "authDomain": "testy-64346.firebaseapp.com",
+  "projectId": "testy-64346",
+  "storageBucket": "testy-64346.appspot.com",
+  "messagingSenderId": "448793626916",
+  "appId": "1:448793626916:web:5adbb1413560821d468bb1",
+  "measurementId": "G-9090BKGJD0"
+}
+
 
 def teacherLogin(request):
-    
+
     return render(request, 'marks.html')
